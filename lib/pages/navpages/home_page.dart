e43ec8b64ec3a37d201dcbcea89f17fab186e2e3
badadarr <badar.maulana.techno@gmail.com>
2022-01-03 23:41:43 +0700

    Create style tabbar

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -8,9 +8,11 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
+
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -42,6 +44,22 @@ class _HomePageState extends State<HomePage> {
           ),
           SizedBox(height: 30),
           // tabbar
+          Container(
+            child: TabBar(controller: _tabController, tabs: [
+              Tab(text: "Places"),
+              Tab(text: "Inspiration"),
+              Tab(text: "Emotions"),
+            ]),
+          ),
+          Container(
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(controller: _tabController, children: [
+              Text("Hi"),
+              Text("There"),
+              Text("Bye"),
+            ]),
+          )
         ],
       ),
     );
