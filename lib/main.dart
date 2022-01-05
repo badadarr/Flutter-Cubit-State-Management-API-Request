80f5d508b371f3925d01997e584a6f5a46670100
badadarr <badar.maulana.techno@gmail.com>
2022-01-05 11:30:35 +0700

    Restructure project folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit/pages/navpages/main_pages.dart';
 
+import 'pages/detail_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,6 +19,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const Mainpage());
+        home: const DetailPage());
   }
 }
---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 20,
+              top: 70,
+              child: Row(
+                children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.menu_rounded))
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
